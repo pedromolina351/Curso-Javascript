972d698fa781d51fe7fb84ebb96a7c93981bc61b
pedromolina351 <pedroalejandromolina90@gmail.com>
2023-05-23 15:12:29 -0600

    cadenas

--- 2. Variables/variables.js
+++ 2. Variables/variables.js
@@ -17,14 +17,34 @@ console.log(edad);
  * undefined: Su valor no está definido.
  */
 var edad = 27;
-let nombre = 'Pedro';
-nombre = 'Juan'; //Al ser una variable let, su valor puede cambiar
+let usuario = 'Pedro';
+usuario = 'Juan'; //Al ser una variable let, su valor puede cambiar
 /*Las cadenas de texto pueden tener comillas simples o dobles, 
 no existe ninguna restricción, pero se usan mucho las simples */
 const correo = 'pedro@correo.com';
 //correo = 'correo2@correo.com' esto es inválido.
 
-console.log(nombre)
+console.log(usuario);
+
+//Ejemplo de como usuar arreglos
+/**Podríamos usar
+ * let nombre;
+ * let apellido;
+ * let telefono;
+ * Pero en lugar de esto, podemos hacer una
+ * sola definición para todas las variables con*/
+let nombre, apellido, telefono;
+telefono = 123456;
+console.log(telefono);
+
+// Operaciones matemáticas en javascript
+const resultado = 4 + 4;
+console.log(resultado);
+// Concatenar cadenas de texto con '+'
+const nombre1 = 'Carlos ';
+const nombre2 = 'Arturo';
+const nombreCompleto = nombre1 + nombre2;
+console.log(nombreCompleto);
 
 
 
