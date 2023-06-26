61f4d05bebfcd48a984c3f05f631403b253de221
pedromolina351 <pedroalejandromolina90@gmail.com>
2023-06-25 21:56:28 -0600

    calculadora

--- Ejemplo1/controlador.js
+++ Ejemplo1/controlador.js
@@ -9,5 +9,20 @@ function sumar() {
 
     // Mostrar el resultado en el elemento con id "resultado"
     document.getElementById("resultado").innerHTML =
+        "El resultado de la suma es: " + resultado;
+}
+
+function restar() {
+    // Obtener los valores de los input
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+
+    // Realizar la suma
+    var resultado = num1 - num2;
+    console.log(resultado);
+
+    // Mostrar el resultado en el elemento con id "resultado"
+    document.getElementById("resultado").innerHTML =
         "El resultado es: " + resultado;
 }
+
